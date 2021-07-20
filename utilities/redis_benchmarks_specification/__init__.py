0d506e0a0d31b5d939789e7109cfd6d3c7a94567
filipecosta90 <filipecosta.90@gmail.com>
2021-07-20 23:44:14 +0100

    api app first working version

---
+++ utilities/redis_benchmarks_specification/__init__.py
@@ -0,0 +1,16 @@
+#  Apache License Version 2.0
+#
+#  Copyright (c) 2021., Redis Labs
+#  All rights reserved.
+#
+
+# This attribute is the only one place that the version number is written down,
+# so there is only one place to change it when the version number changes.
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution(
+        "redis-benchmarks-specification"
+    ).version
+except (pkg_resources.DistributionNotFound, AttributeError):
+    __version__ = "99.99.99"  # like redis
