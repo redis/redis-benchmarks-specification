e636c022fdea8578091bb466fe0178b0f3e4789b
filipecosta90 <filipecosta.90@gmail.com>
2021-09-24 13:34:43 +0100

    [fix] Fixed wrong identation on cli test

--- utils/tests/test_cli.py
+++ utils/tests/test_cli.py
@@ -23,20 +23,20 @@ def test_run_local_command_logic_oss_cluster():
     except SystemExit as e:
         assert e.code == 1
 
-        # should error due to missing --use-tags or --use-branch
-        parser = argparse.ArgumentParser(
-            description="test",
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        )
-        parser = spec_cli_args(parser)
-        TST_REDIS_DIR = os.getenv("TST_REDIS_DIR", None)
-        run_args = ["--use-tags"]
-        if TST_REDIS_DIR is not None:
-            run_args.extend(["--redis_repo", TST_REDIS_DIR])
-        args = parser.parse_args(
-            args=run_args,
-        )
-        try:
-            cli_command_logic(args, "tool", "v0")
-        except SystemExit as e:
-            assert e.code == 0
+    # should error due to missing --use-tags or --use-branch
+    parser = argparse.ArgumentParser(
+        description="test",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser = spec_cli_args(parser)
+    TST_REDIS_DIR = os.getenv("TST_REDIS_DIR", None)
+    run_args = ["--use-tags"]
+    if TST_REDIS_DIR is not None:
+        run_args.extend(["--redis_repo", TST_REDIS_DIR])
+    args = parser.parse_args(
+        args=run_args,
+    )
+    try:
+        cli_command_logic(args, "tool", "v0")
+    except SystemExit as e:
+        assert e.code == 0
