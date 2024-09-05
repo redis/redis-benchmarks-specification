89540182c78df260ce098e1579d8b4e3e643e0c9
fcosta_oliveira <filipe@redis.com>
2024-09-05 23:57:33 +0100

    Included HGETALL 50 fields use-case

--- utils/create-intset.py
+++ utils/create-intset.py
@@ -8,7 +8,7 @@ command = sys.argv[1]
 count = int(sys.argv[2])
 
 command_str = f"{command} int:{count}"
-for x in range(1, count+1):
+for x in range(1, count + 1):
     randint = random.randint(1, 1000000)
     command_str = command_str + f" {randint}"
 
